57dc28aeedcd6e01aa30268562051272c54ab689
Yahor Harmilin <tgarmilina@gmail.com>
2025-07-02 22:23:02 -0400

    Task 2: moved credentials from application.yaml to .env

--- src/main/java/com/javarush/jira/JiraRushApplication.java
+++ src/main/java/com/javarush/jira/JiraRushApplication.java
@@ -12,6 +12,7 @@ import org.springframework.cache.annotation.EnableCaching;
 public class JiraRushApplication {
 
     public static void main(String[] args) {
+        System.out.println("DataSource URL: " + System.getenv("DB_URL"));
         SpringApplication.run(JiraRushApplication.class, args);
     }
 }
